fb556b595e0ddbae3d4bc08a29cc1b0c2b4b81e2
imWildy <119542366+imWildy@users.noreply.github.com>
2025-10-31 18:08:00 +0000

    add: silent rotation to Autism module

--- src/main/java/dev/wildy/mossad/modules/Autism.java
+++ src/main/java/dev/wildy/mossad/modules/Autism.java
@@ -1,7 +1,11 @@
 package dev.wildy.mossad.modules;
 
 import meteordevelopment.meteorclient.events.world.TickEvent;
+import meteordevelopment.meteorclient.settings.BoolSetting;
+import meteordevelopment.meteorclient.settings.Setting;
+import meteordevelopment.meteorclient.settings.SettingGroup;
 import meteordevelopment.meteorclient.systems.modules.Module;
+import meteordevelopment.meteorclient.utils.player.Rotations;
 import meteordevelopment.orbit.EventHandler;
 
 import java.util.Random;
@@ -15,13 +19,26 @@ public class Autism extends Module {
         super(CATEGORY, "autism", "Makes you look autistic");
     }
 
+    private final SettingGroup sgGeneral = this.settings.getDefaultGroup();
+    private final Setting<Boolean> silent = sgGeneral.add(new BoolSetting.Builder()
+        .name("silent-rotations")
+        .description("Silent rotations")
+        .defaultValue(true)
+        .build()
+    );
+
     @EventHandler
     public void onTickPre(TickEvent.Pre ev) {
         float randY = rand.nextFloat() * 360f - 180f;
         float randP = rand.nextFloat() * 180f - 90f;
 
         assert mc.player != null;
-        mc.player.setYaw(randY);
-        mc.player.setPitch(randP);
+
+        if (silent.get()) {
+            Rotations.rotate(randY, randP);
+        } else {
+            mc.player.setYaw(randY);
+            mc.player.setPitch(randP);
+        }
     }
 }
